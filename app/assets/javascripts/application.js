a6fe8f0419978af9cc484fd86a331e62f6a88276
dwarncke <daniel.warncke@gmail.com>
2019-10-16 19:38:50 +0000

    load tasks on page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
+//= require jquery_ujs
 //= require rails-ujs
-//= require activestorage
+//= require turbolinks
 //= require_tree .
+//= require activestorage
+
+
+
+
